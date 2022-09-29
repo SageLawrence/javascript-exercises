4019743a3a7a07f5ff3cc2a8cbdee7b81142c5bf
Sage Lawrence <sageblawrence@gmail.com>
2022-09-29 16:03:37 -0700

    completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
+    let newString = ''; // initializes varaible without introducing 'undefined'
 
+    for (let i = 1; i <= num; i++) { // loops from 1 to concatenate the correct amount of strings
+
+        newString += string;
+
+    };
+
+    if (num < 0){ // sets the error message if the number is negative, comes after the first part to replace if it runs
+
+        newString = 'ERROR';
+
+    }
+
+    return(newString);
+
+}
 // Do not edit below this line
 module.exports = repeatString;
