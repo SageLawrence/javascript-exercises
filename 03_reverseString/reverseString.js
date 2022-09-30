b937ef87502a9dce8cb19fc57c67540f09fac8b9
Sage Lawrence <sageblawrence@gmail.com>
2022-09-29 17:55:06 -0700

    completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    stringArray = string.split(''); //moves to array of characters
+
+    let reverseArray = []; //initialize before loop
+
+    for (let i = 0; i < string.length; i++) { //<length as indices start from zero
+
+       reverseArray[i] = stringArray.pop(); //moves last from original string to i position
+
+    }
+    
+    stringReverse = reverseArray.join(''); //recombination without adding commas inbetween
+
+    return(stringReverse);
 
 };
 
