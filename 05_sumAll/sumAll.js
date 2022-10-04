8a36aab05a4727cca2f954cc19641386cd5a944e
Sage Lawrence <sageblawrence@gmail.com>
2022-10-04 10:50:47 -0700

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(lowInt, highInt) {
+
+    let sum = 0;
+
+    if (lowInt <= 0
+        || highInt <= 0
+        || typeof(lowInt) !== 'number'
+        || typeof(highInt) !== 'number') {
+
+        sum = 'ERROR';
+
+    } else if (lowInt <= highInt) {
+
+        for (i = lowInt; i <= highInt; i++) {
+
+            sum += i;
+
+        }
+    } else {
+
+        for (i = highInt; i <= lowInt; i++)
+        sum += i;
+
+    }
+
+    return(sum);
 
 };
 
