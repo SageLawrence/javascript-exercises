bf8945e940497683f0406a95d104d037014b65cc
Sage Lawrence <sageblawrence@gmail.com>
2022-10-04 12:33:30 -0700

    completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let isLeapYear = true;
+
+    if (year % 4 !== 0          // not leap year if not divisible by 4
+        || year % 100 === 0     // not leap year if divisible by 100 but not 400
+        && year % 400 !== 0) {
+
+        isLeapYear = false;
+
+    }
+
+    return(isLeapYear);
 
 };
 
