fe7d3e635280b0930349a9045aa469c13b8e95a2
Sage Lawrence <sageblawrence@gmail.com>
2022-10-03 17:41:30 -0700

    completed lesson 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) { // ...args accepts multiple inputs for removal
+
+    const removedValues = Array.from(args); // converts removed into an actual array with ALL array properties
+
+    for (const removed of removedValues) { // moves through the array of what you'd like to remove
+
+        for (const index of array) { // for each removed, iterates through the array
+
+            if (removed === index) { // checks equality for removal
+
+                array.splice((array.indexOf(index)),1); // removes value at the index of each value that matches a removed value
+
+            }
+        }
+    }
+
+    return(array); // returns the edited array
 
 };
 
