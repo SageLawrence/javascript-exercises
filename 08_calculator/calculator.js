0de1753d2e7ebc4e920e0afaa78ce7f2ad07994b
Sage Lawrence <sageblawrence@gmail.com>
2022-11-08 21:46:39 -0800

    completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(items) {
+	sumArray = items.reduce((total, item) => total + item, 0);
+  return(sumArray);
 };
 
-const multiply = function() {
-
+const multiply = function(items) {
+	multiplyArray = items.reduce((total, item) => total * item, 1); // starts at one so that multiplication works
+  return(multiplyArray);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+  if (num === 0 || num === 1) {
+    num = 1;
+  } else {
+
+    for (let i = num; i > 1; i--) {
+      num = num * (i - 1);
+    }
+  }
+  return num;
 };
+
 
 // Do not edit below this line
 module.exports = {
