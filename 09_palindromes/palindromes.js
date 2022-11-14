6f2b3d4ecedc87b3db534b70064f04ae7fc944c2
Sage Lawrence <sageblawrence@gmail.com>
2022-11-13 21:14:32 -0800

    completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,43 @@
-const palindromes = function () {
+const palindromes = function (input) {
+
+    let array = input.split(''); // converts the string to an array
+
+    for (i = 0; i < array.length; i++) { // loops through array items
+
+        if (array[i] === '.' // selects spaces and punctuation
+        || array[i] === '!'
+        || array[i] === ','
+        || array[i] === '?'
+        || array[i] === ' ') {
+
+            index = array.indexOf(array[i]); // finds the index of the first index of the selected punctuation
+
+            array.splice(index, 1); // removes punctuation
+
+            i--; // sets i back by one if something is removed so that nothing is skipped
+        } else {
+
+            array[i] = array[i].toLowerCase(); // sets all letters in the array to lowercase
+
+        }
+
+    };
+
+    let result = true; // initialized as true, only takes one fail to make false
+
+    for (i = 0; i <= (array.length)/2 - 1; i++) { // counts halfway through values in array, skips center value if count is odd
+
+        if (array[i] !== array[array.length - i - 1]) { // compares last to first and moves inward
+
+            result = false; // sets result false if values don't match
+
+            break; // stops loop if set to false
+
+        }
+
+    }
+
+    return(result);
 
 };
 
